667ccb28121078402077d9516cfe8875fc31235c
Evert van Harten <evharten@snelmail.nl>
2023-05-08 08:52:25 +0200

    update of index

--- index.php
+++ index.php
@@ -7,11 +7,16 @@ include("competities.php");
 <title>Competitie stickers</title>
 </head>
 <body>
-<form action="pdf.php">
+<form action="pdf.php" method="POST">
 <table>
 <tr><td>Competitie</td><td>
 <select name="aantal">
-
+<?php
+foreach($competities AS $compName => $aantal)
+{
+	echo "<option value=\"$aantal\">$compName</option>\n";
+}
+?>
 </select>
 </td></tr>
 <tr><td>Naam</td><td><input type="name" size=40></td></tr>
