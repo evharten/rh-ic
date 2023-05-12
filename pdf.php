f794cf4e5957e0be7a27ecbf0c82007705b6dc4d
Evert van Harten <evharten@snelmail.nl>
2023-05-12 16:50:00 +0200

    Updated shizzle

--- pdf.php
+++ pdf.php
@@ -1,54 +1,25 @@
 <?php
-
 include("competities.php");
-
 require_once("fpdf/fpdf.php");
+require('pdfLabel.php');
 
 $competitie = $competities[$_POST['competitie']];
 $compNaam = $competitie[0];
 $aantal = $competitie[1];
+
+
 $naam = $_REQUEST['naam'];
 $knsanummer = $_REQUEST['knsa'];
+$page = 0;
 
-$pdf=new FPDF('P','mm','A4');
-$pdf->AliasNbPages();
+// $pdf = new PDF_Label(array('paper-size'=>'A4', 'metric'=>'mm', 'marginLeft'=>6, 'marginTop'=>12, 'NX'=>3, 'NY'=>8, 'SpaceX'=>1, 'SpaceY'=>0, 'width'=>70, 'height'=>35, 'font-size'=>12));
+$pdf = new PDF_Label('staples');
 $pdf->AddPage();
-$page++;
 
-$hCount = 0;
-$newY = 0;
-for ($x = 0; $x < 24; $x++)
+for ($x = 0; $x < $aantal; $x++)
 {
-	// 24 labels to go on the paper
-	if ($hCount >= 3)
-	{
-		$pdf->SetX(0);
-		$newY = $pdf->GetY()+10;
-	} else {
-		$pdf->SetX($pdf->GetX()+20);
-		$pdf->SetY($newY);
-	}
-	
-	if (($x+1) <= $aantal)
-	{
-		$pdf->SetFont('Helvetica','B',14);
-		$pdf->Cell(80,10,$compNaam,0,0);
-		$pdf->Ln(5);
-		$pdf->Cell(80,10,"Naam: $naam",0,0);
-		$pdf->Ln(5);
-		$pdf->Cell(80,10,"KNSA: $knsanummer",0,0);
-		$pdf->Ln(5);
-	} else {
-		$pdf->SetFont('Helvetica','B',14);
-		$pdf->Cell(80,10,"",0,0);
-		$pdf->Ln(5);
-		$pdf->Cell(80,10,"",0,0);
-		$pdf->Ln(5);
-		$pdf->Cell(80,10,"",0,0);		
-		$pdf->Ln(5);
-	}
-	$hCount++;
-	
+		$text = sprintf("%s\n%s\n%s", "$compNaam", "Naam: $naam", "KNSA: $knsanummer");
+		$pdf->Add_Label($text);
 }
 
 $pdf->Output("InterneCompetitie_$knsanummer.pdf", 'I');
