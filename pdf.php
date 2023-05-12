faa4fa4faa76bc942eb222460f2e29c6df7d0a45
Evert van Harten <evharten@snelmail.nl>
2023-05-12 12:44:51 +0200

    Updated pdf

--- pdf.php
+++ pdf.php
@@ -15,21 +15,36 @@ $pdf->AliasNbPages();
 $pdf->AddPage();
 $page++;
 
+$hCount = 0;
+
 for ($x = 0; $x < 24; $x++)
 {
 	// 24 labels to go on the paper
+	if ($hCount >= 3)
+	{
+		$pdf->SetX(0);
+	}
+	
 	if (($x+1) <= $aantal)
 	{
 		$pdf->SetFont('Helvetica','B',14);
-		$pdf->Cell(20,10,$compNaam,1,0);
-		$pdf->Cell(20,10,"Naam: $naam",1,0);
-		$pdf->Cell(20,10,"KNSA: $knsanummer",1,0);
+		$pdf->Cell(20,10,$compNaam,0,0);
+		$pdf->Ln(5);
+		$pdf->Cell(20,10,"Naam: $naam",0,0);
+		$pdf->Ln(5);
+		$pdf->Cell(20,10,"KNSA: $knsanummer",0,0);
+		$pdf->Ln(5);
 	} else {
 		$pdf->SetFont('Helvetica','B',14);
-		$pdf->Cell(20,10,"",1,0);
-		$pdf->Cell(20,10,"",1,0);
-		$pdf->Cell(20,10,"",1,0);		
+		$pdf->Cell(20,10,"",0,0);
+		$pdf->Ln(5);
+		$pdf->Cell(20,10,"",0,0);
+		$pdf->Ln(5);
+		$pdf->Cell(20,10,"",0,0);		
+		$pdf->Ln(5);
 	}
+	$hCount++;
+	
 }
 
 $pdf->Output("InterneCompetitie_$knsanummer.pdf", 'I');
