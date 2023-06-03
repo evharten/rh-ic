6fdd1ecfd98d44aa71bc75fe4c704d6378b15615
Evert van Harten <evharten@snelmail.nl>
2023-06-03 08:59:27 +0200

    Start of sp score lists

--- competities.php
+++ competities.php
@@ -1,19 +1,19 @@
 <?php
-$competities[] = array("Luchtpistool","48");
-$competities[] = array("Luchtgeweer", "48");
-$competities[] = array("Klein kaliber pistool 10 meter", "48");
-$competities[] = array("Klein kaliber pistool 25 meter", "48");
-$competities[] = array("Klein kaliber karabijn 12 meter liggend", "48");
-$competities[] = array("Klein kaliber geweer 12 meter liggend", "48");
-$competities[] = array("Groot kaliber pistool 25 meter", "48");
-$competities[] = array("Zwart kruit pistool 25 meter", "6");
-$competities[] = array("Zwart kruit geweer 50 meter", "6");
-$competities[] = array("Service pistool licht", "6");
-$competities[] = array("Service pistool zwaar", "6");
-$competities[] = array("30m1 karabijn 50 m liggend", "6");
-$competities[] = array("Klein kaliber geweer 50 m liggend", "48");
-$competities[] = array("Klein kaliber karabijn 50 m liggend", "48");
-$competities[] = array("Groot kaliber geweer 50 m liggend", "30");
-$competities[] = array("POK (Precisie geweer open klasse)", "30");
+$competities[] = array("Luchtpistool","48","sticker");
+$competities[] = array("Luchtgeweer", "48","sticker");
+$competities[] = array("Klein kaliber pistool 10 meter", "48","sticker");
+$competities[] = array("Klein kaliber pistool 25 meter", "48","sticker");
+$competities[] = array("Klein kaliber karabijn 12 meter liggend", "48","sticker");
+$competities[] = array("Klein kaliber geweer 12 meter liggend", "48","sticker");
+$competities[] = array("Groot kaliber pistool 25 meter", "48","sticker");
+$competities[] = array("Zwart kruit pistool 25 meter", "6","sticker");
+$competities[] = array("Zwart kruit geweer 50 meter", "6","sticker");
+$competities[] = array("Service pistool licht", "6","sp");
+$competities[] = array("Service pistool zwaar", "6","sp");
+$competities[] = array("30m1 karabijn 50 m liggend", "6","sticker");
+$competities[] = array("Klein kaliber geweer 50 m liggend", "48","sticker");
+$competities[] = array("Klein kaliber karabijn 50 m liggend", "48","sticker");
+$competities[] = array("Groot kaliber geweer 50 m liggend", "30","sticker");
+$competities[] = array("POK (Precisie geweer open klasse)", "30","sticker");
 
 ?>
--- pdf.php
+++ pdf.php
@@ -6,22 +6,27 @@ require('pdfLabel.php');
 $competitie = $competities[$_POST['competitie']];
 $compNaam = $competitie[0];
 $aantal = $competitie[1];
-
+$typeComp = $competitie[2];
 
 $naam = $_REQUEST['naam'];
 $knsanummer = $_REQUEST['knsa'];
 $page = 0;
 
-// $pdf = new PDF_Label(array('paper-size'=>'A4', 'metric'=>'mm', 'marginLeft'=>6, 'marginTop'=>12, 'NX'=>3, 'NY'=>8, 'SpaceX'=>1, 'SpaceY'=>0, 'width'=>70, 'height'=>35, 'font-size'=>12));
-$pdf = new PDF_Label('staples');
-$pdf->AddPage();
-
-for ($x = 0; $x < $aantal; $x++)
+if ($typeComp == "sticker")
 {
-		$text = sprintf("%s\n%s\n%s", "$compNaam", "Naam: $naam", "KNSA: $knsanummer");
-		$pdf->Add_Label($text);
-}
+	// $pdf = new PDF_Label(array('paper-size'=>'A4', 'metric'=>'mm', 'marginLeft'=>6, 'marginTop'=>12, 'NX'=>3, 'NY'=>8, 'SpaceX'=>1, 'SpaceY'=>0, 'width'=>70, 'height'=>35, 'font-size'=>12));
+	$pdf = new PDF_Label('staples');
+	$pdf->AddPage();
 
-$pdf->Output("InterneCompetitie_$knsanummer.pdf", 'I');
-	
+	for ($x = 0; $x < $aantal; $x++)
+	{
+			$text = sprintf("%s\n%s\n%s", "$compNaam", "Naam: $naam", "KNSA: $knsanummer");
+			$pdf->Add_Label($text);
+	}
+
+	$pdf->Output("InterneCompetitie_$knsanummer.pdf", 'I');
+}
+ else 
+{
+}	 
 ?>
